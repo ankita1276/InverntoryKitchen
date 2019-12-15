4f888a8fa1c8fa95ef5c27bd49775ec720fbfd22
Ankita Chavan <ankitac@apporchid.com>
2019-12-16 03:57:22 +0530

    feat(completed,ingredients,kithcendisplay):components added

---
+++ src/app/corePages/kitchen-display/kitchen-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen-display',
+  templateUrl: './kitchen-display.component.html',
+  styleUrls: ['./kitchen-display.component.scss']
+})
+export class KitchenDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
