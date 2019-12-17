37ba0356421a76960c24fbd6c5c4c1eee1fffe47
Ankita Chavan <ankitac@apporchid.com>
2019-12-17 16:43:23 +0530

    added api data to kitchendisplay

--- src/app/corePages/kitchen-display/kitchen-display.component.ts
+++ src/app/corePages/kitchen-display/kitchen-display.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/shared/services/commonService';
-import { element } from 'protractor';
 
 @Component({
   selector: 'app-kitchen-display',
@@ -9,13 +8,11 @@ import { element } from 'protractor';
 })
 export class KitchenDisplayComponent implements OnInit {
 
-  orderData : any
+  orderData : Object[];
 
   constructor(private _commonService_ : CommonService) { }
 
   ngOnInit() {
-    console.log("called");
-    
     this._commonService_.getService().subscribe((res)=>{
       let array = [];
       Object.keys(res).forEach(element => {array.push(res[element])})
